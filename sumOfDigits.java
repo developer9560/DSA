75afaf9bbdc634d37c4d457be914759ec7845f77
developer9560 <developer95608@gmail.com>
2025-01-18 23:51:53 +0530

    solve find the sum of digits

---
+++ sumOfDigits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class sumOfDigits {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+        int totalSum = 0;
+        while (num>0) {
+            totalSum +=num%10;
+           num = num/10; 
+        }
+
+    System.out.println("sum of digits is : "+totalSum);
+    }
+}
