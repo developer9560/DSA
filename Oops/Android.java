6c22ad185d64bdd141e546447b5c9f2edb1d24a0
developer9560 <developer95608@gmail.com>
2025-04-19 03:37:26 +0530

    learning about inheritance and use single and multiple inheritance

---
+++ Oops/Android.java
@@ -0,0 +1,10 @@
+public class Android extends Mobiles {
+    String android_version;
+
+    public String getAndroidVersion() {
+        return android_version;
+    }
+    public void setAndroidVersion(String android_version) {
+        this.android_version = android_version;
+    }
+}
---
+++ Oops/Mobiles.java
@@ -0,0 +1,24 @@
+public class Mobiles {
+    int cost;
+    String ram;
+    String color;
+
+    public int getCost(){
+        return cost;
+    }
+    public void setCost(int cost){
+        this.cost = cost;
+    }
+    public String getRam(){
+        return ram;
+    }
+    public void setRam(String ram){
+        this.ram = ram;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+}
